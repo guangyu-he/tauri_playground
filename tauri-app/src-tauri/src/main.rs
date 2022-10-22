47dcf575fb1b2d992edaa098d91f8050cc1cdfb6
Guangyu He <fredhgy2010@gmail.com>
2022-10-22 16:45:39 +0200

    update own codes

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -9,9 +9,44 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn roman_to_int(s: String) -> i32 {
+    let mut sum: i32 = 0;
+    let mut last: i32 = 0;
+    for c in s.chars().rev() {
+        let current: i32 = roman_char_to_int(c);
+        if current >= last {
+            sum += current;
+        } else {
+            sum -= current;
+        }
+        last = current;
+    }
+    sum
+}
+
+fn roman_char_to_int(c: char) -> i32 {
+    match c {
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000,
+        _ => 0,
+    }
+}
+
+#[tauri::command]
+fn append_vec(a: Vec<i32>, b: Vec<i32>) -> Vec<i32> {
+    println!("{:#?}", a);
+    a.into_iter().chain(b.into_iter()).collect()
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, roman_to_int,append_vec])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -1,16 +1,48 @@
-const { invoke } = window.__TAURI__.tauri;
+const {invoke} = window.__TAURI__.tauri;
 
 let greetInputEl;
 let greetMsgEl;
+let romanInputEl;
+let romanMsgEl;
+let listInput1;
+let listInput2;
+let listMsg;
+let array1;
+let array2;
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
+    greetInputEl = document.querySelector("#greet-input");
+    greetMsgEl = document.querySelector("#greet-msg");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    romanInputEl = document.querySelector("#roman-input");
+    romanMsgEl = document.querySelector("#roman-msg");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    listInput1 = document.querySelector("#list1-input");
+    listInput2 = document.querySelector("#list2-input");
+    array1 = JSON.parse("[" + listInput1 + "]");
+    array2 = JSON.parse("[" + listInput2 + "]");
+    listMsg = document.querySelector("#list-msg");
 });
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {name: greetInputEl.value});
 }
 
+async function roman_to_int() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    romanMsgEl.textContent = await invoke("roman_to_int", {s: romanInputEl.value});
+}
+
+async function merge_list() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    listMsg.textContent = await invoke("append_vec", {a: array1, b: array2});
+}
+
+window.roman_to_int = roman_to_int;
 window.greet = greet;
+window.merge_list = merge_list;
